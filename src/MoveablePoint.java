5f7f070b0c2fda31ecaab62dbdfeb2f4edf9f6d8
unknown <khanhbi1998pt@gmail.com>
2022-03-14 10:46:47 +0700

    BT

---
+++ src/MoveablePoint.java
@@ -0,0 +1,56 @@
+public class MoveablePoint extends Point {
+    float xSpeed = 0.0f;
+    float ySpeed = 0.0f;
+
+    public MoveablePoint(){
+    }
+
+    public MoveablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed){
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed(){
+        float[] f = {xSpeed,ySpeed};
+        return f;
+    }
+
+    @Override
+    public String toString() {
+        return "MoveablePoint{" +
+                "xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                "} " + super.toString();
+    }
+    public float[] MoveablePoint(){
+        setXY(getX()+ xSpeed,getY()+ySpeed);
+        return getXY();
+    }
+}
